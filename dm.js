e58370d09a9cd63b4740336c88ccee68c64a22c0
ankienho <89306359+ankienho@users.noreply.github.com>
2022-10-13 01:54:44 +0700

    Update dm.js

--- dm.js
+++ dm.js
@@ -1,4 +1,30 @@
 function checkthisSound (){
     var sound = document.getElementById("audio");
-    sound.src = "srcpic\\dumam.mp3"
+    sound.src = "srcpic\\hayhayxutxac.mp3"
+}
+
+
+function setClor(){
+    document.getElementsById("text").style.color = 'red';
+    
+}
+
+function deom (){
+    var sound = document.getElementById("audio");
+    sound.src = "srcpic\\deom.mp3"
+}
+
+
+function hienhinh(){
+    let sinh = document.getElementById("sinh");
+    sinh.src = "srcpic\\sinh.jpg"
+}
+
+function luamne (){
+    var sound1 = document.getElementById("lua");
+    sound1.src = "srcpic\\dumam.mp3"
+    setTimeout(() => {
+        document.write("m bị nhừa gòi")
+        alert("lêu lêu")
+    }, 3000);
 }
